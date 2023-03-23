a6768a4c33e822b7c561cd6f491006648ac7b3e1
RonVol <rvolnets@gmail.com>
2023-03-23 20:52:25 +0200

    Q&A

--- js/main.js
+++ js/main.js
@@ -6,3 +6,28 @@ function changeNav() {
       x.className = "topNav";
     }
   }
+
+function sendEmail() {
+  let email = document.getElementById("email").value;
+  let subject = document.getElementById("subject").value;
+  let body = document.getElementById("email-content").value;
+
+  let mailtoLink = "mailto:ronvo@post.bgu.ac.il?subject=" + encodeURIComponent(subject) + "&body=" + encodeURIComponent(body);
+
+  window.location.href = mailtoLink;
+}
+
+function addEmoji(emoji) {
+  let inputEle = document.getElementById("email-content");
+  
+  inputEle.value += emoji;
+}
+
+function toggleEmojiDrawer() {
+  let drawer = document.getElementById('drawer');
+  if (drawer.classList.contains('hidden')) {
+    drawer.classList.remove('hidden');
+  } else {
+    drawer.classList.add('hidden');
+  }
+}
