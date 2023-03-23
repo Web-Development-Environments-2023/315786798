1bc69e849200d4dbade72a2f6dffd0a38b0a24ce
RonVol <rvolnets@gmail.com>
2023-03-23 18:58:23 +0200

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function changeNav() {
+    var x = document.getElementById("topNav");
+    if (x.className === "topNav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topNav";
+    }
+  }
